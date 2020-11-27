30127a92232f6512b028218855f2d9b060522e96
David Li <xxinli.1983@gmail.com>
2020-11-28 08:12:01 +1100

    table display

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import fetch from 'node-fetch';
 import logo from './logo.svg';
 import './App.css';
+import { CompanyTable } from './components/dashboard';
 
 class App extends Component {
   state = {
@@ -16,14 +17,9 @@ class App extends Component {
 }
   // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
 callBackendAPI = async () => {
-  const response = await fetch('/company');
-  console.log(response);
-  const body = await response.json();
-
-  if (response.status !== 200) {
-    throw Error(body.message) 
-  }
-  return body;
+  return fetch('/company', {
+    method: 'get'
+  }).then(res => res.json());
 };
 
 render() {
@@ -34,7 +30,17 @@ render() {
         <p>
           Edit11 <code>src/App.js</code> and save to reload.
         </p>
-        
+        <CompanyTable
+        items={[
+          { id: 1, name: 'Cheese', price: 4.9, stock: 20 },
+          { id: 2, name: 'Milk', price: 1.9, stock: 32 },
+          { id: 3, name: 'Yoghurt', price: 2.4, stock: 12 },
+          { id: 4, name: 'Heavy Cream', price: 3.9, stock: 9 },
+          { id: 5, name: 'Butter', price: 0.9, stock: 99 },
+          { id: 6, name: 'Sour Cream ', price: 2.9, stock: 86 },
+          { id: 7, name: 'Fancy French Cheese 🇫🇷', price: 99, stock: 12 },
+        ]}
+      />
         <a
           className="App-link"
           href="https://reactjs.org"
